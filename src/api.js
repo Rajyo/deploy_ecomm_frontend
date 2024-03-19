4ce5257aae204462502f934fb83a35400178ebc2
Prajyot Khadse <yopro1999@gmail.com>
2024-03-19 16:17:27 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -12,6 +12,6 @@ export const api = axios.create({
 
 export default class ApiService {
   static saveStripeInfo(data = {}) {
-    return api.post(`${API_URL}/payments/save-stripe-info/`, data);
+    return api.post(`/payments/save-stripe-info/`, data);
   }
 }
